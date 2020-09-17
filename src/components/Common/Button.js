5e2782cddab8e0c0db4c7fff18eca5611230ba40
masoud94 <masoud.solid@gmail.com>
2020-09-17 15:57:03 +0430

    button custom

--- src/components/Common/Button.js
+++ src/components/Common/Button.js
@@ -2,11 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
 
-const _Button = ({ type, size, onClick, children }) => (
-  <Button onClick={onClick} size={size} type={type}>
-    {children}
-  </Button>
-);
+const _Button = ({ type, size, onClick, children, ghost, loading, light, gray, icon }) => {
+  let styler = {};
+  if (gray) {
+    styler = {
+      backgroundColor: 'whitesmoke',
+      color: '#b8b8b8',
+      borderColor: '#b8b8b8',
+    };
+  }
+  if (light)
+    styler = {
+      opacity: '65%',
+    };
+
+  return (
+    <Button
+      className={'flex justify-center items-center'}
+      onClick={onClick}
+      size={size}
+      type={type}
+      ghost={ghost}
+      loading={loading}
+      style={styler}
+      icon={icon}
+    >
+      {children}
+    </Button>
+  );
+};
 
 _Button.propTypes = {
   type: PropTypes.string,
