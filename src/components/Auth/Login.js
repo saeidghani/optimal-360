99657bb937f646bd4521da6e24c9e567d5e3b2e6
ehsan <ehsan.ahmadi.work@gmail.com>
2020-09-17 11:10:31 +0430

    feat(*) : added example for custom less

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -4,7 +4,7 @@ import Button from '../Common/Button';
 import Checkbox from '../Common/Checkbox';
 
 const Login = (props) => (
-  <div className="grid grid-rows-3 grid-flow-col gap-4 p-5">
+  <div className="grid grid-rows-3 grid-flow-col gap-4 p-5 custom-css-style">
     <div className="row-span-3 bg-red-200">Hi</div>
     <div className="row-span-1 col-span-2 bg-teal-400">Hi</div>
     <div className="row-span-2 col-span-2 bg-green-600">hi</div>
