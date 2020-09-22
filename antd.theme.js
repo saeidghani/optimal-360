a0de63c9d252e27ab9e1a40454001294a874ee29
ehsan <ehsan.ahmadi.work@gmail.com>
2020-09-22 14:21:53 +0330

    feat(*) : set base-font-size to 14 for all

--- antd.theme.js
+++ antd.theme.js
@@ -1,8 +1,12 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 
+// const colors = {
+//   'primary-color': '#1DA57A'
+// }
+
 module.exports = {
-  // 'primary-color': '#1DA57A',
+  'primary-color': '#1BB7D9',
   // 'link-color': '#1DA57A',
   // 'border-radius-base': '2px',
-  'font-size-base': '16',
+  'font-size-base': '14',
 };
