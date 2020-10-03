0b9922eb829e912d7f45a14483e0cafe6d3fb5e6
AmirHossein Khademi <amirhossein.kahdemi@rdsysco.com>
2020-10-03 10:18:10 +0330

    FEAT(survey): Implement basic ui for rater verification email

--- src/components/Projects/RaterVerificationEmail.jsx
+++ src/components/Projects/RaterVerificationEmail.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import Checkbox from '../Common/Checkbox';
 import MainLayout from '../Common/Layout';
-import Menu from './Menu';
-import Steps from '../Common/Steps';
-import DatePicker from '../Common/DatePicker';
 import Button from '../Common/Button';
-import { Table } from 'antd';
-import { useHistory } from 'react-router-dom';
 
 const RaterVerificationEmail = () => {
   return (
     <MainLayout title="Super User" contentClass="pt-4" titleClass="pl-6">
       <div className="bg-white w-full flex font-sans ">
-        <Menu />
-        <div className="w-full pl-6 pt-6  ">
-          <Steps className="w-full" />
-          <div className="mt-16 flex flex-col pr-33"></div>
+        <div className="w-full pt-12 px-6.5 ">
+          <span className="text-secondary text-20px ">Rater verification email</span>
+          <div className="mt-2.5 flex flex-row justify-between items-center">
+            <div className="flex">
+              <Button
+                text="Project Name"
+                className="text-base px-3 h-8 flex items-center justify-center mr-3 rounded-2px"
+                // onClick={() => history.push('/super-user/Projects/rater-verification-email')}
+              />
+              <Button
+                text="Rater"
+                className="text-base w-16 h-8 flex items-center justify-center mr-3 rounded-2px"
+                // onClick={() => history.push('/super-user/Projects/rater-verification-email')}
+              />
+              <Button
+                text="Sender"
+                className="text-base w-19 h-8 flex items-center justify-center rounded-2px"
+                // onClick={() => history.push('/super-user/Projects/rater-verification-email')}
+              />
+            </div>
+            <div className=" flex">
+              <Button
+                type="link"
+                text="Cancel"
+                className="text-base w-24.5 h-9.5 flex items-center justify-center"
+              />
+              <Button
+                text="Save"
+                className="text-base w-24.5 h-9.5 flex items-center justify-center rounded-2px"
+                // onClick={() => history.push('/super-user/Projects/rater-verification-email')}
+              />
+            </div>
+          </div>
+          <div className="h-94 w-full bg-template rounded-7px  mt-12">sd</div>
         </div>
       </div>
     </MainLayout>
--- src/containers/Projects/EmailSetting.jsx
+++ src/containers/Projects/EmailSetting.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Projects/EmailSetting';
 const EmailSetting = () => {
-  return <Layout></Layout>;
+  return <Layout />;
 };
 
 export default EmailSetting;
