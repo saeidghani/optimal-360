364b8a6654a53f1c471ad37bffd6c947f9ff2705
ehsan <ehsan.ahmadi.work@gmail.com>
2020-10-13 19:00:27 +0330

    feat(logic) : survey setting
    - added confirmation modal for switching between surveyGroups
    - added manual logic for touched & error & values for form compoenents

--- src/components/Wizard/Helper/Menu.js
+++ src/components/Wizard/Helper/Menu.js
@@ -3,28 +3,11 @@ import PropTypes from 'prop-types';
 import { Menu } from 'antd';
 import { useQuery } from '../../../hooks/useQuery';
 
-const _Menu = ({ items, title, className }) => {
-  const [parsedQuery, , setQuery] = useQuery();
+const _Menu = ({ items, title, className, onClick }) => {
+  const [parsedQuery] = useQuery();
 
   const sortedArr = items?.sort((el1, el2) => el1.id - el2.id);
   // in order to keep menu items order consistent across renders
-
-  React.useEffect(() => {
-    const firstSurveyGroupId = sortedArr?.length > 0 ? sortedArr[0].id : '';
-
-    const isURLSurveyGroupValid = !!sortedArr.find(
-      (el) => el.id?.toString() === parsedQuery?.surveyGroupId?.toString(),
-    );
-
-    if (
-      !isURLSurveyGroupValid &&
-      firstSurveyGroupId &&
-      firstSurveyGroupId !== parsedQuery?.surveyGroupId
-    ) {
-      setQuery({ surveyGroupId: firstSurveyGroupId });
-    }
-    // eslint-disable-next-line
-  }, []);
 
   return (
     <Menu
@@ -37,7 +20,7 @@ const _Menu = ({ items, title, className }) => {
       {sortedArr.map(({ id, name }) => (
         <Menu.Item
           className="inline-flex items-center text-sm leading-5 capitalize text-antgray-100 my-2"
-          onClick={({ key }) => setQuery({ surveyGroupId: key })}
+          onClick={({ key }) => onClick(key)}
           key={id}
         >
           {name}
@@ -51,6 +34,7 @@ _Menu.propTypes = {
   items: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string,
   className: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 _Menu.defaultProps = {
