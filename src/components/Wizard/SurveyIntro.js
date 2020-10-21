a526b050bda2117afa7fb155a1f7331c901f0ff9
mehrdad <javadabbasi72@gmail.com>
2020-10-21 13:31:53 +0330

    resolve conflict

--- src/components/Wizard/SurveyIntro.js
+++ src/components/Wizard/SurveyIntro.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainLayout from '../Common/Layout';
 import Menu from './Helper/Menu';
 import Steps from '../Common/Steps';
 import Button from '../Common/Button';
+import UploadAvatar from '../Common/UploadAvatar';
 
 const SurveyIntro = () => {
+
+  const [imageFile, setImageFile] = useState('');
+
+  const getImageFile = (file) => {
+    setImageFile(file);
+  }
+
   return (
-    <MainLayout title="Super User" contentClass="pt-4" titleClass="pl-6">
-      <div className="bg-white w-full flex font-sans ">
-        <Menu />
-        <div className="w-full px-6 pt-6  ">
-          <Steps className="w-full" />
+    <MainLayout
+      hasBreadCrumb
+      title="Super User"
+      titleClass="mb-2"
+      contentClass="py-4"
+      headerClassName="pl-21"
+      childrenPadding={false}
+    >
+      <div className="bg-white grid grid-cols-12 pl-15">
+        <Menu className="col-span-2" />
+        <div className="px-6 py-5 col-start-3 col-span-10  ">
+          <Steps currentPosition={2} />
           <div className="mt-13 flex flex-row items-center">
-            <span className=" ml-8 mr-3 text-body  text-base">Client picture</span>
-            <Button
-              text="Upload"
-              className="bg-white rounded-2px text-primary-500 border-primary-500 text-base font-normal box-border"
-            />
+            {/* <span className=" ml-8 mr-3 text-body  text-base">Client picture</span> */}
+            <UploadAvatar selectedFile={getImageFile} image="" />
           </div>
           <div className=" mt-18 pr-28">
             <span className="text-body text-base">Client Welcome Message</span>
             <div className="mt-3.5 bg-template rounded-7px w-full h-53 mb-18">sd</div>
             <span className="text-body text-base ">Survey Message</span>
             <div className="mt-3.5 bg-template rounded-7px w-full h-53">sd</div>
+            <div className="pt-10 pb-22  flex justify-end">
+              <Button type="link" text="Back" />
+              <Button text="Next" />
+            </div>
           </div>
         </div>
       </div>
