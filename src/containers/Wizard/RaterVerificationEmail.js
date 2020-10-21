a0d10583e31715c08eb80a3083de19f65e3bbc0b
mehrdad <javadabbasi72@gmail.com>
2020-10-21 14:17:49 +0330

    resolve conflict

--- src/containers/Wizard/RaterVerificationEmail.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from '../../components/Wizard/RaterVerificationEmail';
-
-const RaterVerificationEmail = () => {
-  return <Layout />;
-};
-
-export default RaterVerificationEmail;
