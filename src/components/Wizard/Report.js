525c778d9b18f9d0679a9a29012a02c781f03bcc
Fazel <mahdifazeli@live.com>
2020-10-23 15:10:24 +0330

    fix report handler

--- src/components/Wizard/Report.js
+++ src/components/Wizard/Report.js
@@ -6,12 +6,81 @@ import Button from '../Common/Button';
 import TextEditor from '../Common/TextEditor';
 import Checkbox from '../Common/Checkbox';
 
-
 const SurveyIntro = () => {
+  const initialReportList = [
+    {
+      id: 1,
+      lable: 'Report content:',
+      isChecked: false,
+    },
+    {
+      id: 2,
+      lable: 'Competency Results:',
+      isChecked: false,
+    },
+    {
+      id: 3,
+      lable: 'Client Competency Model:',
+      isChecked: false,
+    },
+    {
+      id: 4,
+      lable: 'Behavior Results:',
+      isChecked: false,
+    },
+    {
+      id: 5,
+      lable: 'Raters Information:',
+      isChecked: false,
+    },
+    {
+      id: 6,
+      lable: 'Feedback/Comments (Development Areas):',
+      isChecked: false,
+    },
+    {
+      id: 7,
+      lable: 'Feedback/Comments (General):',
+      isChecked: false,
+    },
+    {
+      id: 8,
+      lable: 'Results Overview:',
+      isChecked: false,
+    },
+    {
+      id: 9,
+      lable: 'Development Plan:',
+      isChecked: false,
+    },
+    {
+      id: 10,
+      lable: 'Competency Level and Awareness:',
+      isChecked: false,
+    },
+    {
+      id: 11,
+      lable: 'Notes Page:',
+      isChecked: false,
+    },
+    {
+      id: 12,
+      lable: 'Include Previous Results Data:',
+      isChecked: false,
+    },
+    {
+      id: 13,
+      lable: 'Include Mission Critical Data:',
+      isChecked: false,
+    },
+  ];
+  const [reportList, setReportList] = useState(initialReportList);
 
-  const onChangeCheckbox = (event) => {
-    console.log("onChangeCheckbox -> event", event)
-  }
+  const onChangeCheckbox = (id, val) => {
+    const newTeportList = [...reportList];
+    newTeportList.map((x) => (x.id === id) && (x.isChecked = val));
+    setReportList(newTeportList);
+  };
 
   return (
     <MainLayout
@@ -26,45 +95,27 @@ const SurveyIntro = () => {
         <Menu className="col-span-2" />
         <div className="px-6 py-5 col-start-3 col-span-10  ">
           <Steps currentPosition={4} />
-          <div className="flex mt-12 mb-3" >
+          <div className="flex mt-12 mb-3">
             <Button text="Individual Report" className="mr-3" />
             <Button text="Group Report" className="" />
           </div>
           <div>
             <div className="flex flex-wrap">
-              <div className="bg-gray-100 px-10 py-5 w-full">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Report content:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Competency Results:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Client Competency Model:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Behavior Results:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Raters Information:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Feedback/Comments (Development Areas):</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Feedback/Comments (General):</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Results Overview:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Development Plan:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Competency Level and Awareness:</Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/2 border-b border-gray-200">
-                <Checkbox labelClass="text-secondary" onChange={(event) => onChangeCheckbox(event)}>Notes Page:</Checkbox>
-              </div>
+              {reportList.map((item) => (
+                item.id < 12 &&
+                (
+                  <div className="bg-gray-100 px-10 py-5 w-full" key={item.id}>
+                    <Checkbox
+                      labelClass="text-secondary"
+                      onChange={(value) => onChangeCheckbox(item.id, value)}
+                      checked={item.isChecked}
+                    >
+                      {item.lable}
+                    </Checkbox>
+                  </div>
+                )
+              ))}
+
               <div className="w-full mt-16 px-10">
                 <TextEditor
                   placeholder="Client Competency Model"
@@ -77,10 +128,22 @@ const SurveyIntro = () => {
               </p>
 
               <div className="px-10 py-5 w-1/4">
-                <Checkbox labelClass="text-secondary font-medium" onChange={(event) => onChangeCheckbox(event)}>Include Previous Results Data:</Checkbox>
+                <Checkbox
+                  labelClass="text-secondary font-medium"
+                  onChange={(value) => onChangeCheckbox(reportList[11].id, value)}
+                  checked={reportList[11].isChecked}
+                >
+                  {reportList[11].lable}
+                </Checkbox>
               </div>
               <div className="px-10 py-5 w-1/4">
-                <Checkbox labelClass="text-secondary font-medium" onChange={(event) => onChangeCheckbox(event)}>Include Mission Critical Data:</Checkbox>
+                <Checkbox
+                  labelClass="text-secondary font-medium"
+                  onChange={(value) => onChangeCheckbox(reportList[12].id, value)}
+                  checked={reportList[12].isChecked}
+                >
+                  {reportList[12].lable}
+                </Checkbox>
               </div>
             </div>
 
