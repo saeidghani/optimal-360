7b11a44b6ec99c882046df776bda896400d2b89e
saeid <saeidghani@gmail.com>
2020-10-24 23:00:26 +0330

    added survey groups organizations

---
+++ src/containers/SurveyGroups/Organizations.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+import Layout from '../../components/SurveyGroups/Organizations';
+
+class Organizations extends Component {
+  state = {};
+
+  render() {
+    const { loading } = this.props;
+
+    return <Layout loading={loading} />;
+  }
+}
+
+Organizations.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  loading: state.loading.global || false,
+});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Organizations);
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ import SetAdmin from '../containers/Projects/SetAdmin';
 
 import SurveyGroupList from '../containers/SurveyGroups/List';
 import RatersStatusOverview from '../containers/SurveyGroups/RatersStatusOverview';
+import Organizations from '../containers/SurveyGroups/Organizations';
 
 import ProjectInfo from '../containers/Wizard/ProjectInfo';
 import SurveySetting from '../containers/Wizard/SurveySetting';
@@ -61,6 +62,11 @@ const Index = () => (
       path="/super-user/new-project/report"
       exact
       component={Report}
+    />
+    <PrivateRoute
+      path="/super-user/projects/survey-groups"
+      exact
+      component={Organizations}
     />
     <CustomRoute path="/" exact component={TestingArea} />
     <Route component={NotFound} />
