0709a3aff0574a1fbb4aa41040190caaf17f161a
saeid <saeidghani@gmail.com>
2020-10-25 17:39:46 +0330

    added organizations new

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ import SetAdmin from '../containers/Projects/SetAdmin';
 
 import SurveyGroupList from '../containers/SurveyGroups/List';
 import RatersStatusOverview from '../containers/SurveyGroups/RatersStatusOverview';
+import NewOrganizations from '../containers/SurveyGroups/NewOrganizations';
 
 import ProjectInfo from '../containers/Wizard/ProjectInfo';
 import SurveySetting from '../containers/Wizard/SurveySetting';
@@ -61,6 +62,11 @@ const Index = () => (
       path="/super-user/new-project/report"
       exact
       component={Report}
+    />
+    <PrivateRoute
+      path="/super-user/organizations/new"
+      exact
+      component={NewOrganizations}
     />
     <CustomRoute path="/" exact component={TestingArea} />
     <Route component={NotFound} />
