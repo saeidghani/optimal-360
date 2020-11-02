3ac5d9ab3e5cd63f3c3b11fc53619fdc95bce406
saeid <saeidghani@gmail.com>
2020-11-02 17:05:34 +0330

    added individual route and container

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,6 +33,7 @@ import SurveyPlatformForgotPassword from '../containers/SurveyPlatform/ForgotPas
 import SurveyPlatformWelcome from '../containers/SurveyPlatform/Welcome';
 import Information from '../containers/SurveyPlatform/Information';
 import AllRatees from '../containers/SurveyPlatform/AllRatees';
+import Individual from '../containers/SurveyPlatform/Individual';
 
 import NotFound from '../components/404';
 import TestingArea from '../components/TestingArea';
@@ -109,6 +110,7 @@ const Index = () => (
     <PrivateRoute path="/survey-platform/welcome" exact component={SurveyPlatformWelcome} />
     <PrivateRoute path="/survey-platform/information" exact component={Information} />
     <PrivateRoute path="/survey-platform/managers/all-ratees" exact component={AllRatees} />
+    <PrivateRoute path="/survey-platform/managers/individual" exact component={Individual} />
 
     <PrivateRoute path="/super-user/organizations/users/new" exact component={NewOrganizations} />
     <CustomRoute path="/" exact component={TestingArea} />
