73f5e808b3927eafef1fca2b5bba693852939834
hesamkaveh <hesamkaveh@joolico.com>
2020-11-03 16:33:02 +0330

    reOrdered routes

--- src/routes/SuperUser.js
+++ src/routes/SuperUser.js
@@ -36,44 +36,34 @@ import NotFound from '../components/404';
 
 const Routes = ({ match }) => (
   <Switch>
-    <CustomRoute path={`${match.path}/login`} exact component={Login} />
-    <CustomRoute path={`${match.path}/forgot-password`} exact component={ForgotPassword} />
+    {/* authentication */}
+    <CustomRoute
+      path={`${match.path}/login`}
+      exact
+      component={Login}
+    />
+    <CustomRoute
+      path={`${match.path}/forgot-password`}
+      exact
+      component={ForgotPassword}
+    />
 
-    <PrivateRoute path={`${match.path}/projects`} exact component={ProjectsList} />
-    <PrivateRoute path={`${match.path}/projects/:projectId/set-admin`} exact component={SetAdmin} />
-
+    {/* projects */}
+    <PrivateRoute
+      path={`${match.path}/projects`}
+      exact
+      component={ProjectsList}
+    />
+    <PrivateRoute
+      path={`${match.path}/projects/:projectId/set-admin`}
+      exact
+      component={SetAdmin}
+    />
     <PrivateRoute
       path={`${match.path}/projects/:projectId/survey-groups`}
       exact
       component={SurveyGroupList}
     />
-
-    <PrivateRoute
-      path={`${match.path}/participants/status-overview`}
-      exact
-      component={RatersStatusOverview}
-    />
-    <PrivateRoute
-      path={`${match.path}/participants/status-details`}
-      exact
-      component={RatersStatusDetails}
-    />
-    <PrivateRoute
-      path={`${match.path}/participants/rater-email`}
-      exact
-      component={RatersStatusRaterEmail}
-    />
-    <PrivateRoute
-      path={`${match.path}/participants/individual-report`}
-      exact
-      component={RatersStatusIndividualReport}
-    />
-    <PrivateRoute
-      path={`${match.path}/participants/group-report-review`}
-      exact
-      component={RatersStatusGroupReportReview}
-    />
-
     <PrivateRoute
       path={`${match.path}/new-project/project-info`}
       exact
@@ -110,30 +100,74 @@ const Routes = ({ match }) => (
       scrollToTop
       component={SurveyQuestions}
     />
-    <PrivateRoute path={`${match.path}/new-project/report`} exact scrollToTop component={Report} />
+    <PrivateRoute
+      path={`${match.path}/new-project/report`}
+      exact
+      scrollToTop
+      component={Report}
+    />
     <PrivateRoute
       path={`${match.path}/new-project/reports/group-reports`}
       exact
       scrollToTop
       component={GroupReports}
     />
+    <PrivateRoute
+      path={`${match.path}/projects/survey-groups`}
+      exact
+      component={Organizations}
+    />
 
-    <PrivateRoute path={`${match.path}/projects/survey-groups`} exact component={Organizations} />
-    <PrivateRoute path={`${match.path}/organizations/:organizationId/users`} exact component={OrganizationsUsers} />
+    {/* participants */}
+    <PrivateRoute
+      path={`${match.path}/participants/status-overview`}
+      exact
+      component={RatersStatusOverview}
+    />
+    <PrivateRoute
+      path={`${match.path}/participants/status-details`}
+      exact
+      component={RatersStatusDetails}
+    />
+    <PrivateRoute
+      path={`${match.path}/participants/rater-email`}
+      exact
+      component={RatersStatusRaterEmail}
+    />
+    <PrivateRoute
+      path={`${match.path}/participants/individual-report`}
+      exact
+      component={RatersStatusIndividualReport}
+    />
+    <PrivateRoute
+      path={`${match.path}/participants/group-report-review`}
+      exact
+      component={RatersStatusGroupReportReview}
+    />
+
+    {/* organizations */}
+    <PrivateRoute
+      path={`${match.path}/organizations/:organizationId/users`}
+      exact
+      component={OrganizationsUsers}
+    />
     <PrivateRoute
       path={`${match.path}/organizations/:organizationId/new-staff`}
       exact
       component={OrganizationsNewStaff}
     />
-
-    <PrivateRoute path={`${match.path}/bank/models`} exact component={BankModels} />
-
+    <PrivateRoute
+      path={`${match.path}/bank/models`}
+      exact
+      component={BankModels}
+    />
     <PrivateRoute
       path={`${match.path}/organizations/users/new`}
       exact
       component={NewOrganizations}
     />
 
+    {/* other */}
     <Route component={NotFound} />
   </Switch>
 );
