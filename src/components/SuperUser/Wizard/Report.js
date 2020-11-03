ddf4a4e85b6c80c720ee3412b907c1b191778345
ehsan <ehsan.ahmadi.work@gmail.com>
2020-11-03 18:22:06 +0330

    feat(api) : added api integration

--- src/components/SuperUser/Wizard/Report.js
+++ src/components/SuperUser/Wizard/Report.js
@@ -1,86 +1,138 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { Formik, Form } from 'formik';
+import * as yup from 'yup';
+
+import { useQuery, stringify } from '../../../hooks/useQuery';
+import { useSurveyGroup } from '../../../hooks';
+
+import ChangeSurveyGroupModal from './Helper/ChangeSurveyGroupModal';
+
 import MainLayout from '../../Common/Layout';
+import Loading from '../../Common/Loading';
 import Menu from './Helper/Menu';
 import Steps from '../../Common/Steps';
 import Button from '../../Common/Button';
 import TextEditor from '../../Common/TextEditor';
 import Checkbox from '../../Common/Checkbox';
 
-const SurveyIntro = () => {
-  const initialReportList = [
-    {
-      id: 1,
-      lable: 'Report content:',
-      isChecked: false,
-    },
-    {
-      id: 2,
-      lable: 'Competency Results:',
-      isChecked: false,
-    },
-    {
-      id: 3,
-      lable: 'Client Competency Model:',
-      isChecked: false,
-    },
-    {
-      id: 4,
-      lable: 'Behavior Results:',
-      isChecked: false,
-    },
-    {
-      id: 5,
-      lable: 'Raters Information:',
-      isChecked: false,
-    },
-    {
-      id: 6,
-      lable: 'Feedback/Comments (Development Areas):',
-      isChecked: false,
-    },
-    {
-      id: 7,
-      lable: 'Feedback/Comments (General):',
-      isChecked: false,
-    },
-    {
-      id: 8,
-      lable: 'Results Overview:',
-      isChecked: false,
-    },
-    {
-      id: 9,
-      lable: 'Development Plan:',
-      isChecked: false,
-    },
-    {
-      id: 10,
-      lable: 'Competency Level and Awareness:',
-      isChecked: false,
-    },
-    {
-      id: 11,
-      lable: 'Notes Page:',
-      isChecked: false,
-    },
-    {
-      id: 12,
-      lable: 'Include Previous Results Data:',
-      isChecked: false,
-    },
-    {
-      id: 13,
-      lable: 'Include Mission Critical Data:',
-      isChecked: false,
-    },
-  ];
-  const [reportList, setReportList] = useState(initialReportList);
+const LABELS = {
+  contentPage: 'Content Page',
+  competencyResults: 'Competency Results',
+  clientCompetencyModel: 'Client Competency Model',
+  averageScoreByCompetency: 'Average Score by Competency',
+  behaviorResults: 'Behavior Results',
+  ratersInformation: 'Raters Information',
+  developmentFeedbackComment: 'Feedback/Comments (Development Areas)',
+  missionCriticalCompetencies: 'Mission Critical Competencies',
+  generalFeedbackComment: 'Feedback/Comments (General)',
+  resultsOverview: 'Results Overview',
+  developmentPlan: 'Development Plan',
+  competencyLevelAndAwareness: 'Competency Level and Awareness',
+  notesPage: 'Notes Page',
 
-  const onChangeCheckbox = (id, val) => {
-    const newTeportList = [...reportList];
-    newTeportList.map((x) => x.id === id && (x.isChecked = val));
-    setReportList(newTeportList);
-  };
+  missionCriticalCompetenciesByParticipants: 'Mission Critical Competencies by Participants',
+  surveyCompletionStatus: 'Survey Completion Status',
+  participantGapAnalysis: 'Participant Gap Analysis',
+  groupResultsOverview: 'Group Results Overview',
+  participantOverviewByCompetency: 'Participant Overview by Competency',
+  averageScoreByCluster: 'Average Score by Cluster',
+  participantOverviewByBehaviors: 'Participant Overview by Behaviors',
+};
+
+const Report = ({ reports, fetchReports, setReports, loading }) => {
+  const schema = yup.object({
+    individualReport: yup.object({
+      competencyModelTemplate: yup.string().required('Client competency model is required'),
+    }),
+    groupReport: yup.object({
+      competencyModelTemplate: yup.string().required('Client competency model is required'),
+    }),
+  });
+
+  const formRef = React.useRef();
+  const history = useHistory();
+
+  const [parsedQuery, query, setQuery] = useQuery();
+  const { projectId } = parsedQuery;
+
+  const [surveyGroups, currentSurveyGroupName, surveyGroupId] = useSurveyGroup();
+
+  const [surveyGroupModal, setSurveyGroupModal] = React.useState(false);
+  const [isFormDone, setIsFormDone] = React.useState(false);
+  const [selectedSurveyGroupKey, setSelectedSurveyGroupKey] = React.useState('');
+
+  React.useEffect(() => {
+    if (!parsedQuery?.reportType) {
+      setQuery({
+        reportType: 'individualReport',
+      });
+    }
+  }, [query]);
+
+  React.useEffect(() => {
+    if (surveyGroupId) fetchReports(surveyGroupId);
+  }, [projectId, surveyGroupId, fetchReports]);
+
+  React.useEffect(() => {
+    const resetForm = async () => {
+      await fetchReports(surveyGroupId);
+
+      if (formRef?.current) {
+        // reset form state when surveyGroup changes
+        // happens when user decides to discard current settings and changes currentSurveyGroup
+        formRef.current.setTouched({});
+        formRef.current.setErrors({});
+        formRef.current.setValues({ ...formRef?.current?.values });
+      }
+    };
+
+    if (surveyGroupId) {
+      resetForm();
+    }
+
+    // eslint-disable-next-line
+  }, [fetchReports, surveyGroupId]);
+
+  React.useEffect(() => {
+    if (
+      isFormDone &&
+      selectedSurveyGroupKey &&
+      selectedSurveyGroupKey !== parsedQuery?.surveyGroupId
+    ) {
+      setQuery({ surveyGroupId: selectedSurveyGroupKey });
+      setIsFormDone(false);
+      setSurveyGroupModal(false);
+    }
+  }, [isFormDone, selectedSurveyGroupKey, setQuery, parsedQuery.surveyGroupId]);
+
+  React.useEffect(() => {
+    const validateForm = async () => {
+      try {
+        const errorObj = await formRef.current.validateForm(formRef?.current?.values);
+
+        if (errorObj && Object.values(errorObj).length > 0) {
+          throw errorObj;
+        } else {
+          setIsFormDone(true);
+        }
+      } catch (errorObj) {
+        formRef.current.setErrors(errorObj);
+        formRef.current.setTouched(errorObj);
+
+        if (selectedSurveyGroupKey !== parsedQuery?.surveyGroupId) setSurveyGroupModal(true);
+      }
+    };
+
+    if (selectedSurveyGroupKey && formRef?.current) {
+      validateForm(formRef?.current?.values);
+    }
+
+    // eslint-disable-next-line
+  }, [selectedSurveyGroupKey]);
+
+  const { reportType = '' } = parsedQuery || {};
 
   return (
     <MainLayout
@@ -91,68 +143,193 @@ const SurveyIntro = () => {
       headerClassName="pl-21"
       childrenPadding={false}
     >
+      <Loading visible={loading} />
+
+      <ChangeSurveyGroupModal
+        handleOk={() => {
+          setIsFormDone(true);
+        }}
+        handleCancel={() => {
+          setSelectedSurveyGroupKey('');
+          setSurveyGroupModal(false);
+        }}
+        currentSurveyGroup={currentSurveyGroupName}
+        visible={surveyGroupModal}
+      />
+
       <div className="bg-white grid grid-cols-12 pl-15">
-        <Menu className="col-span-2" />
+        <Menu
+          onClick={(key) => setSelectedSurveyGroupKey(key)}
+          isFormDone={isFormDone}
+          items={surveyGroups?.data}
+          className="col-span-2"
+        />
+
         <div className="px-6 py-5 col-start-3 col-span-10  ">
           <Steps currentPosition={4} />
-          <div className="flex mt-12 mb-3">
-            <Button text="Individual Report" className="mr-3" />
-            <Button text="Group Report" className="" />
-          </div>
-          <div>
-            <div className="flex flex-wrap">
-              {reportList.map(
-                (item) =>
-                  item.id < 12 && (
-                    <div className="px-10 py-5 w-1/2 border-b border-gray-200" key={item.id}>
-                      <Checkbox
-                        labelClass="text-secondary"
-                        onChange={(value) => onChangeCheckbox(item.id, value)}
-                        checked={item.isChecked}
-                      >
-                        {item.lable}
-                      </Checkbox>
-                    </div>
-                  ),
-              )}
 
-              <div className="w-full mt-16 px-10">
-                <TextEditor
-                  placeholder="Client Competency Model"
-                  label="Client Competency Model:"
-                />
-              </div>
+          <Formik
+            innerRef={formRef}
+            enableReinitialize
+            initialValues={reports}
+            validationSchema={schema}
+            onSubmit={async (values) => {
+              try {
+                await setReports({ surveyGroupId, ...values });
+                const params = stringify({ projectId, surveyGroupId });
 
-              <p className="w-full mt-12 px-10 text-base font-medium">Additional Report Setting:</p>
+                history.push(`/super-user/participants/status-overview${params}`);
+              } catch (error) {}
+            }}
+          >
+            {({ values, errors, touched, handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <div className="flex mt-12 mb-3">
+                  <Button
+                    size="middle"
+                    textSize="xs"
+                    text="Individual Report"
+                    className="mr-3"
+                    onClick={() => setQuery({ reportType: 'individualReport' })}
+                    light={parsedQuery?.reportType !== 'individualReport'}
+                  />
+                  <Button
+                    size="middle"
+                    textSize="xs"
+                    text="Group Report"
+                    onClick={() => setQuery({ reportType: 'groupReport' })}
+                    light={parsedQuery?.reportType === 'individualReport'}
+                  />
+                </div>
 
-              <div className="px-10 py-5 w-1/4">
-                <Checkbox
-                  labelClass="text-secondary font-medium"
-                  onChange={(value) => onChangeCheckbox(reportList[11].id, value)}
-                  checked={reportList[11].isChecked}
-                >
-                  {reportList[11].lable}
-                </Checkbox>
-              </div>
-              <div className="px-10 py-5 w-1/4">
-                <Checkbox
-                  labelClass="text-secondary font-medium"
-                  onChange={(value) => onChangeCheckbox(reportList[12].id, value)}
-                  checked={reportList[12].isChecked}
-                >
-                  {reportList[12].lable}
-                </Checkbox>
-              </div>
-            </div>
+                <div className="grid grid-cols-12 mt-3">
+                  <div className="col-span-12 flex flex-row items-center bg-antgray-700 py-5 px-8.3">
+                    <Checkbox
+                      className="flex flex-row items-center"
+                      onChange={(value) => {
+                        const newValues = { ...values };
 
-            <div className="pt-23.5 pb-22 flex justify-end pr-33">
-              <Button text="Submit" />
-            </div>
-          </div>
+                        Object.keys(newValues[reportType]).forEach((key) => {
+                          if (LABELS[key]) {
+                            newValues[reportType][key] = value;
+                          }
+                        });
+
+                        formRef.current.setValues({ ...newValues });
+                      }}
+                      checked={
+                        !Object.entries(values[reportType] || {}).find(
+                          ([key, value]) =>
+                            !!(LABELS[key] && typeof value === 'boolean' && value === false),
+                        )
+                      }
+                      textNode={
+                        <p className="antgray-100 text-xs leading-4 ml-8 whitespace-no-wrap">
+                          Report content:
+                        </p>
+                      }
+                    />
+                  </div>
+
+                  {Object.entries(values[reportType] || {}).map(([key, value]) => {
+                    return LABELS[key] ? (
+                      // eslint-disable-next-line react/jsx-indent
+                      <div key={key} className="col-span-6 py-5 px-8.3 flex flex-row items-center">
+                        <Checkbox
+                          className="flex flex-row items-center"
+                          onChange={(newVal) => {
+                            const newValues = { ...values };
+
+                            newValues[reportType][key] = newVal;
+
+                            formRef.current.setValues({ ...newValues });
+                          }}
+                          checked={value}
+                          textNode={
+                            <p className="antgray-100 text-xs leading-4 ml-8 whitespace-no-wrap">
+                              {LABELS[key]}
+                            </p>
+                          }
+                        />
+                      </div>
+                    ) : null;
+                  })}
+                </div>
+
+                <div className="flex flex-wrap">
+                  <TextEditor
+                    wrapperClassName="w-full mt-16"
+                    value={values[reportType]?.competencyModelTemplate}
+                    onChange={(val) => {
+                      const newValues = { ...values };
+
+                      if (newValues[reportType]) {
+                        newValues[reportType].competencyModelTemplate = val;
+
+                        formRef.current.setValues({ ...newValues });
+                      }
+                    }}
+                    placeholder="Client Competency Model"
+                    label="Client Competency Model :"
+                  />
+                  <p className="text-red-500 h-5">
+                    {touched.competencyModelTemplate && errors.competencyModelTemplate}
+                  </p>
+
+                  <p className="w-full mt-12 text-base font-medium">Additional Report Setting:</p>
+
+                  <Checkbox
+                    className="flex flex-row items-center py-5"
+                    checked={!!values[reportType]?.includePreviousResults}
+                    onChange={(val) => {
+                      const newValues = { ...values };
+
+                      newValues[reportType].includePreviousResults = val;
+
+                      formRef.current.setValues({ ...newValues });
+                    }}
+                    textNode={<p>Include Previous Results Data</p>}
+                  />
+
+                  <Checkbox
+                    className="flex flex-row items-center py-5"
+                    checked={!!values[reportType]?.includeMissionCriticalData}
+                    onChange={(val) => {
+                      const newValues = { ...values };
+
+                      newValues[reportType].includeMissionCriticalData = val;
+
+                      formRef.current.setValues({ ...newValues });
+                    }}
+                    textNode={<p>Include Mission Critical Data</p>}
+                  />
+                </div>
+
+                <div className="pt-23.5 pb-22 flex justify-end pr-33">
+                  <Button className="w-24.5 h-9.5" onClick={handleSubmit} text="Submit" />
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </MainLayout>
   );
 };
 
-export default SurveyIntro;
+Report.propTypes = {
+  fetchReports: PropTypes.func.isRequired,
+  setReports: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  reports: PropTypes.shape({
+    clientPicture: PropTypes.string,
+    clientWelcomeMessage: PropTypes.string,
+    surveyMessage: PropTypes.string,
+  }),
+};
+
+Report.defaultProps = {
+  reports: { clientPicture: '', clientWelcomeMessage: '', surveyMessage: '' },
+};
+
+export default Report;
