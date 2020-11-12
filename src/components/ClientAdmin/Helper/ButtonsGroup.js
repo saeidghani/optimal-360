f1c02104a62556afe00df56fffc8119f75460ce3
saeid <saeidghani@gmail.com>
2020-11-12 23:31:04 +0330

    fixed all client admin ui bugs

--- src/components/ClientAdmin/Helper/ButtonsGroup.js
+++ src/components/ClientAdmin/Helper/ButtonsGroup.js
@@ -5,9 +5,9 @@ import Button from '../../Common/Button';
 
 const ButtonsTab = ({ activeButtonKey, wrapperClassName, buttonClassName, disabled }) => {
   const viewButtons = [
-    { key: '1', title: 'View Ratee Summary' },
-    { key: '2', title: 'View Ratee Details' },
-    { key: '3', title: 'View Rater Details' },
+    { key: '1', title: 'View Ratee Summary', disabled: true },
+    { key: '2', title: 'View Ratee Details', disabled: true },
+    { key: '3', title: 'View Rater Details', disabled: false },
   ];
 
   return (
@@ -15,12 +15,14 @@ const ButtonsTab = ({ activeButtonKey, wrapperClassName, buttonClassName, disabl
       {viewButtons.map((button) => (
         <Button
           key={button.key}
-          className={`mr-4 border-list-border shadow-none px-2 md:px-4 w-full
-            ${button.key === activeButtonKey ? 'bg-primary' : 'bg-white'} ${buttonClassName}`}
-          textClassName={`${button.key === activeButtonKey ? 'text-white' : 'text-heading'}`}
+          className={`c-force-padding-y-px mr-4 border-list-border shadow-none px-2 md:px-4 w-full
+            ${button.key === activeButtonKey ? 'bg-primary' : 'bg-white'}
+            ${button.disabled ? '' : 'text-white bg-primary-500'} ${buttonClassName}`}
+          textClassName={`pt-4 ${button.disabled ? '' : 'text-white'} ${buttonClassName}`}
           textSize="sm"
           text={button.title}
-          disabled={disabled}
+          disabled={button.disabled}
+          onClick={() => {}}
         />
       ))}
     </div>
