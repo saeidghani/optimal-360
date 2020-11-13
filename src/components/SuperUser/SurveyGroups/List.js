9c62589c5eb3c0cab8aea052715de3e89f99d3be
hesamkaveh <hesamkaveh@joolico.com>
2020-11-13 13:47:26 +0330

    changed link to ratee

--- src/components/SuperUser/SurveyGroups/List.js
+++ src/components/SuperUser/SurveyGroups/List.js
@@ -124,10 +124,10 @@ const SurveyGroups = ({ loading }) => {
       {
         key: 'name',
         title: 'Survey Group',
-        render: (name) => (
+        render: (name, { project, surveyGroupId }) => (
           <Button
             className="pl-0"
-            onClick={() => history.push('/super-user/participants/ratee')}
+            onClick={() => history.push(`/super-user/participants/ratee?projectId=${project.id}&surveyGroupId=${surveyGroupId}`)}
             type="link"
             textSize="sm"
             text={name}
