d32a1861dff621ae0dc956fee16a81bc0e97d1bb
hesamkaveh <hesamkaveh@joolico.com>
2020-11-18 16:10:24 +0330

    rename argument

--- src/hooks/useTabs.js
+++ src/hooks/useTabs.js
@@ -1,19 +1,19 @@
 import { useQuery } from './useQuery';
 
-const useTabs = (tabParam = 'tab', tabArray) => {
+const useTabs = (tabKeyParam = 'tab', tabArray) => {
   const [parsedQuery, , setQuery] = useQuery();
   const allTabs = tabArray;
-  const currentTab = parsedQuery?.[tabParam]?.toString() || allTabs[0];
+  const currentTab = parsedQuery?.[tabKeyParam]?.toString() || allTabs[0];
 
   const setTab = (tab) => {
     if (allTabs.includes(tab)) {
-      setQuery({ [tabParam]: tab, page_size: '10', sort: '', page_number: '1', q: '' });
+      setQuery({ [tabKeyParam]: tab, page_size: '10', sort: '', page_number: '1', q: '' });
     } else {
-      setQuery({ [tabParam]: allTabs[0], page_size: '10', sort: '', page_number: '1', q: '' });
+      setQuery({ [tabKeyParam]: allTabs[0], page_size: '10', sort: '', page_number: '1', q: '' });
     }
   };
 
-  return [currentTab, setTab, tabParam];
+  return [currentTab, setTab, tabKeyParam];
 };
 
 // eslint-disable-next-line import/prefer-default-export
