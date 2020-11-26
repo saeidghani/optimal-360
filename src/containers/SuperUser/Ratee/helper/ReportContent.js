998be3dfd32253d67a895768ba4cc5c292370358
hesamkaveh <hesamkaveh@joolico.com>
2020-11-26 19:50:56 +0330

    working on report content

--- src/containers/SuperUser/Ratee/helper/ReportContent.js
+++ src/containers/SuperUser/Ratee/helper/ReportContent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 import { useQuery } from "../../../../hooks";
 import { Checkbox } from "antd";
+import { Form, Formik } from "formik";
 
 const CheckboxGroup = Checkbox.Group;
 const LABELS = {
@@ -30,12 +31,15 @@ const LABELS = {
   participantOverviewByCompetency: 'Participant Overview by Competency',
   averageScoreByCluster: 'Average Score by Cluster',
   participantOverviewByBehaviors: 'Participant Overview by Behaviors',
+  bb: 'false by default',
 };
-const data = {
+const sample = {
   individual: {
     INTRODUCTION: {
       contentPage: true,
       competencyResults: false,
+      aa: true,
+      bb: false,
     },
     RESULTSOVERVIEW: {
       clientCompetencyModel: false,
@@ -45,7 +49,7 @@ const data = {
   group: {
     title_11: {
       ratersInformation: true,
-      developmentFeedbackComment: false,
+      developmentFeedbackComment: true,
     },
     title_12: {
       missionCriticalCompetencies: false,
@@ -54,78 +58,123 @@ const data = {
   },
 };
 const ReportContent = (loading) => {
+  const formRef = React.useRef();
+
+  const isLevel1Selected = (category) => {
+    let selectedAll = true;
+    Object.entries(formRef?.current?.values?.[category] || []).forEach(([key, value]) => {
+      if (Object.values(value).includes(false)) {
+        selectedAll = false;
+      }
+    });
+    return selectedAll;
+  };
+
+  const data = React.useMemo(() => {
+    const newObj = { ...sample };
+    Object.entries(newObj).forEach(([key, value]) => {
+      Object.entries(value).forEach(([key2, value2]) => {
+        if (LABELS[key2]) {
+          Object.entries(value2).forEach(([key3, value3]) => {
+            if (!LABELS[key3]) {
+              delete newObj?.[key]?.[key2]?.[key3];
+            }
+          });
+        } else {
+          delete newObj?.[key]?.[key2];
+        }
+      });
+    });
+    return newObj;
+  }, [sample]);
 
   return (
     <>
       <div>
-        <div
-          className="col-span-12 flex flex-row items-center bg-antgray-600 py-5 px-8.3 border-b border-t border-antgray-900">
-          <div className="w-full">
-            <Checkbox
-              className="flex flex-row items-center text-antgray-100"
-              onChange={() => {
-              }}
-              checked={false}
-            >
-              Report content: Individual
-            </Checkbox>
-          </div>
-          <div className="w-full">
-            <Checkbox
-              className="flex flex-row items-center text-antgray-100"
-              onChange={() => {
-              }}
-              checked={false}
-            >
-              Report content: Group
-            </Checkbox>
-          </div>
-        </div>
-
-        <div className="flex">
-          {['individual', 'group'].map((category) => (
-            <div className="flex flex-col w-1/2">
-              {/* eslint-disable-next-line array-callback-return */}
-              {Object.entries(data[category]).map(([_title, fields]) => {
-                  if (LABELS[_title]) {
-                    return (
-                      <div key={_title} className={`checkbox-group-wrapper ${category === 'group' && 'ml-0'}`}>
-                        <Checkbox
-                          className="text-heading text-base"
-                          indeterminate={() => {
-                          }}
-                          onChange={() => {
-                          }}
-                          checked={() => {
-                          }}
-                        >
-                          {LABELS[_title]}
-                        </Checkbox>
-
-                        <CheckboxGroup
-                          className="checkbox-group-container text-heading text-sm"
-                          options={(Object.entries(fields).filter(([label]) => LABELS[label])).map(([label]) => {
-                            return ({
-                              label: LABELS[label],
-                              value: label,
+        <Formik
+          innerRef={formRef}
+          enableReinitialize
+          initialValues={data}
+          onSubmit={async (values) => {
+          }}
+        >
+          {({ values, handleSubmit }) => (
+            <Form onSubmit={handleSubmit}>
+              <>
+                <div
+                  className="col-span-12 flex flex-row items-center bg-antgray-600 py-5 px-8.3 border-b border-t border-antgray-900">
+                  {['individual', 'group'].map((category) => (
+                    <div className="w-full" key={category}>
+                      <Checkbox
+                        className="flex flex-row items-center text-antgray-100"
+                        onChange={() => {
+                          const stack = [];
+                          Object.entries(values[category]).forEach(([key, value]) => {
+                            stack.push(...Object.values(value));
+                          });
+                          const selectTo = stack.includes(false);
+                          const newValues = { ...values };
+                          Object.entries(values[category]).forEach(([key, value]) => {
+                            Object.entries(value).forEach(([key3, value3]) => {
+                              newValues[category][key][key3] = selectTo;
                             });
-                          })}
-                          value={Object.entries(fields).filter(([_, value]) => (value === true)).map(([aa, _]) => aa)}
-                          onChange={(a) => {
-                            console.log(a);
-                          }}
-                        />
-                      </div>
-                    );
-                  }
-                },
-              )}
+                          });
+                          formRef.current.setValues({ ...newValues });
+                        }}
+                        checked={isLevel1Selected(category)}
+                      >
+                        Report content: Individual
+                      </Checkbox>
+                    </div>
+                  ))}
+                </div>
 
-            </div>
-          ))}
+                <div className="flex">
+                  {['individual', 'group'].map((category) => (
+                    <div className="flex flex-col w-1/2" key={category}>
+                      {Object.entries(values[category]).map(([_title, fields]) => (
+                          <div key={_title} className={`checkbox-group-wrapper ${category === 'group' && 'ml-0'}`}>
+                            <Checkbox
+                              className="text-heading text-base"
+                              indeterminate={!Object.values(values[category][_title]).every((a) => a === Object.values(values[category][_title])[0])}
+                              onChange={() => {
+                                const newValue = { ...values };
+                                const changeTo = !Object.values(values[category][_title]).every((a) => a === true);
+                                Object.entries(values[category][_title]).forEach(([key]) => {
+                                  newValue[category][_title][key] = changeTo;
+                                });
+                                formRef.current.setValues({ ...values, ...newValue });
+                              }}
+                              checked={Object.values(values[category][_title]).every((a) => a === true)}
+                            >
+                              {LABELS[_title]}
+                            </Checkbox>
 
-
-        </div>
+                            <CheckboxGroup
+                              className="checkbox-group-container text-heading text-sm"
+                              options={(Object.entries(fields).filter(([label]) => LABELS[label])).map(([label]) => ({
+                                label: LABELS[label],
+                                value: label,
+                              }))}
+                              value={Object.entries(fields).filter(([_, value]) => (value === true)).map(([aa, _]) => aa)}
+                              onChange={(selectedArray) => {
+                                const newValue = { ...values };
+                                Object.entries(values[category][_title]).forEach(([property]) => {
+                                  newValue[category][_title][property] = selectedArray.includes(property);
+                                });
+                                formRef.current.setValues({ ...newValue });
+                              }}
+                            />
+                          </div>
+                        ),
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </>
+            </Form>
+          )}
+        </Formik>
       </div>
     </>
   );
